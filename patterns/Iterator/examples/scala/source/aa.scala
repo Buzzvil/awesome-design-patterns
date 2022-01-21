08db8c89925eaea5f110984c9f07c28785d8368f
bronn cha <agyomany@gmail.com>
2022-01-21 14:31:03 +0900

    append scalatest to sbt

--- patterns/Iterator/examples/scala/source/aa.scala
+++
@@ -1,5 +0,0 @@
-object aa {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world")
-  }
-}
---
+++ patterns/Iterator/examples/scala/src/main/bb$.java
@@ -0,0 +1,5 @@
+object bb{
+        def main(args:Array[String]):Unit={
+        println("Hello, world")
+        }
+        }
