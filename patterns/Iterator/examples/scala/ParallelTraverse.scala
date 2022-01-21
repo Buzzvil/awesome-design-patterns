379eb242781e5c9e7ec4cb29d35dd480d72cd3c9
bronn cha <agyomany@gmail.com>
2022-01-21 10:32:53 +0900

    fix errors;

--- patterns/Iterator/examples/scala/ParallelTraverse.scala
+++ patterns/Iterator/examples/scala/ParallelTraverse.scala
@@ -1,12 +1,12 @@
 // Parallel traverse
 
-val list = List(1, 2, 3)
-val iterator1 = list.iterator
-val iterator2 = list.iterator
-
-println(iterator1.next)
-println(iterator1.next)
-println(iterator2.next)
-println(iterator2.next)
-println(iterator2.next)
-println(iterator1.next)
+//val list = List(1, 2, 3)
+//val iterator1 = list.iterator
+//val iterator2 = list.iterator
+//
+//println(iterator1.next)
+//println(iterator1.next)
+//println(iterator2.next)
+//println(iterator2.next)
+//println(iterator2.next)
+//println(iterator1.next)
--- patterns/Iterator/examples/scala/simple.scala
+++ patterns/Iterator/examples/scala/simple.scala
@@ -1,2 +1,2 @@
-val iterator = Iterator(3, 1, 2)
-iterator foreach println
+//val iterator = Iterator(3, 1, 2)
+//iterator foreach println
