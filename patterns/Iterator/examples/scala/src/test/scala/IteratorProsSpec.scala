27aa8b50bd9c4cf5322ad950b08904b4a0f715fb
bronn cha <agyomany@gmail.com>
2022-02-07 21:03:29 +0900

    delay example

--- patterns/Iterator/examples/scala/src/test/scala/IteratorProsSpec.scala
+++ patterns/Iterator/examples/scala/src/test/scala/IteratorProsSpec.scala
@@ -6,18 +6,31 @@ class IteratorProsSpec extends AnyFlatSpec {
   exampleStudentList append Student("C", 13)
   exampleStudentList append Student("B", 12)
 
+  "coder" should "implement new types of iterators and pass them to existing code without breaking anything" in {
+
+  }
+
   "multiple iterators" should "iterate over the same collection in parallel" in {
     val iterator1 = exampleStudentList.iterator
     val iterator2 = exampleStudentList.iterator
 
     assert(iterator1.next().name == "A")
-    assert(iterator1.next().name == "C")
-
     assert(iterator2.next().name == "A")
 
-    assert(iterator1.next().name == "B")
-
+    assert(iterator1.next().name == "C")
     assert(iterator2.next().name == "C")
+
+    assert(iterator1.next().name == "B")
     assert(iterator2.next().name == "B")
+  }
+
+  "Iterator" should "work when delayed" in {
+    val iterator1 = exampleStudentList.iterator
+    assert(iterator1.next().age == 11)
+
+    val iterator2 = exampleStudentList.iterator
+    assert(iterator2.hasNext == true)
+
+    assert(iterator1.next().age == 13)
   }
 }
