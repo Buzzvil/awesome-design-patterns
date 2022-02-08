45248ecad1b29427a0706f2651277c842327bf16
dy.kim <dy.kim@buzzvil.com>
2022-02-08 10:05:44 +0900

    Use private set; kotlin specific

--- examples/Singleton/kotlin/singleton.kt
+++ examples/Singleton/kotlin/singleton.kt
@@ -1,10 +1,7 @@
-// Kotlin은 Java와 다르게 public, private을 쓰지 않고도 singleton을 만들 수 있다 !!
+// Kotlin은 Java와 다르게 public, private을 쓰지 않고도 singleton을 만들 수 있다.
 object Coin {
-  private var coin: Int = 0
-
-  fun getCoin():Int {
-    return coin
-  }
+  var coin: Int = 0
+    private set
 
   fun addCoin() {
     coin += 10
