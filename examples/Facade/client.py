12e3330ad00337ff2e518c60c891424f93662645
Nathan Gyeongmo Yang <nathan.yang@buzzvil.com>
2022-02-14 20:17:25 +0900

    fix: add working example

--- examples/Facade/client.py
+++ examples/Facade/client.py
@@ -1,13 +1,16 @@
 from typing import Sequence
 
-from examples.Facade.subsystem import MenuService, MenuItem, OrderService, Facade
+from subsystem import MenuService, MenuItem, OrderService, Facade
 
 
 class SubSystemAwareClient:
     def order_best(self) -> bool:
-        menu_items: Sequence[MenuItem] = MenuService().get_all_menus()
-        menu_items.sort(lambda item: item.amount)
-        best_menu: MenuItem = menu_items[0]
+        all_menu_items: Sequence[MenuItem] = MenuService().get_all_menus()
+        print(f'Total {len(all_menu_items)} exists.')
+
+        best_menu: MenuItem = MenuService().get_best_menu()
+        print(f'Got best_menu {best_menu}.')
+
         ordered: bool = OrderService().order(best_menu)
         return ordered
 
@@ -16,3 +19,11 @@ class Client:
     def order_best(self) -> bool:
         facade = Facade()
         return facade.order_best()
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.order_best()
+    # >> Total 3 exists.
+    # >> Got best_menu MenuItem(name='Pizza', price='25000', score=5).
+    # >> Ordered MenuItem(name='Pizza', price='25000', score=5)!
--- examples/Facade/subsystem.py
+++ examples/Facade/subsystem.py
@@ -11,18 +11,43 @@ class MenuItem:
 
 class Facade:
     def order_best(self) -> bool:
-        menu_items: Sequence[MenuItem] = MenuService().get_all_menus()
-        menu_items.sort(lambda item: item.amount)
-        best_menu: MenuItem = menu_items[0]
+        all_menu_items: Sequence[MenuItem] = MenuService().get_all_menus()
+        print(f'Total {len(all_menu_items)} exists.')
+
+        best_menu: MenuItem = MenuService().get_best_menu()
+        print(f'Got best_menu {best_menu}.')
+
         ordered: bool = OrderService().order(best_menu)
         return ordered
 
 
 class MenuService:
-    def get_all_menus(self):
-        pass
+    MENUS = (
+        MenuItem(
+            name='Chicken',
+            price='18000',
+            score=4,
+        ),
+        MenuItem(
+            name='Pizza',
+            price='25000',
+            score=5,
+        ),
+        MenuItem(
+            name='Salad',
+            price='12000',
+            score=1,
+        ),
+    )
+
+    def get_all_menus(self) -> Sequence[MenuItem]:
+        return self.MENUS
+
+    def get_best_menu(self) -> Sequence[MenuItem]:
+        return sorted(self.MENUS, key=lambda item: item.score, reverse=True)[0]
 
 
 class OrderService:
-    def order(self, menu_item: MenuItem):
-        pass
+    def order(self, menu_item: MenuItem) -> bool:
+        print(f'Ordered {menu_item}!')
+        return True
