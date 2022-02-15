77125b2e4b3e51dd09ee7e0e8a33015228ff7325
bronn cha <agyomany@gmail.com>
2022-02-15 21:12:25 +0900

    complete cartTest

--- patterns/Iterator/examples/kotlin/src/main/kotlin/Cart.kt
+++ patterns/Iterator/examples/kotlin/src/main/kotlin/Cart.kt
@@ -1,1 +1,5 @@
-class Cart(products: Int)
+class Cart(products: List<Product>) {
+    private val cartProducts = products
+    fun getSize(): Int = cartProducts.size
+    fun getProducts(): List<Product> = listOf()
+}
--- patterns/Iterator/examples/kotlin/src/test/kotlin/CartTests.kt
+++ patterns/Iterator/examples/kotlin/src/test/kotlin/CartTests.kt
@@ -1,17 +1,23 @@
 import io.kotest.core.spec.style.BehaviorSpec
+import kotlin.test.assertEquals
 
 class CartTests: BehaviorSpec({
     given("빈 Cart 객체가 있다") {
-        val emptyCart = Cart(3)
+        val emptyCart = Cart(listOf())
+
         `when` ("Cart 객체에 담긴 상품 수를 요청했다.") {
+            val actualSize = emptyCart.getSize()
+
             then("상품 수는 0이어야 한다.") {
-                assert(true)
+                assertEquals(0, actualSize)
             }
         }
 
         `when` ("Cart 객체에 담긴 상품 리스트를 요청했다.") {
+            val actualList = emptyCart.getProducts()
+
             then("빈 리스트가 반환되어야 한다.") {
-                assert(true)
+                assertEquals(actualList, listOf())
             }
         }
     }
