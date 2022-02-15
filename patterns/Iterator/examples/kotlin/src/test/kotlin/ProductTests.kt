b815e4cd5609687718a0067908a0c8810333486a
bronn cha <agyomany@gmail.com>
2022-02-15 20:56:51 +0900

    complete productTests

--- patterns/Iterator/examples/kotlin/src/test/kotlin/ProductTests.kt
+++ patterns/Iterator/examples/kotlin/src/test/kotlin/ProductTests.kt
@@ -4,17 +4,21 @@ import kotlin.test.assertEquals
 class ProductTests: BehaviorSpec({
     given("이름은 'water'이고 가격은 1000원인 Product 객체가 있다.") {
         val product = Product("water", 1000)
+
         `when`("상품의 이름을 요청했다.") {
+            val actualName = product.name
+
             then("반환된 이름은 'water' 이어야 한다.") {
-                assertEquals("water", product.name)
+                assertEquals("water", actualName)
             }
         }
 
-        `when`("상품의 가격 요청했다.") {
+        `when`("상품의 가격을 요청했다.") {
+            val actualPrice = product.price
+
             then("반환된 가격은 1000원 이어야 한다.") {
-                assertEquals(1000, product.price)
+                assertEquals(1000, actualPrice)
             }
         }
     }
-
 })
