e88a27c8eb0451f22d464cb9c163c7dac8ef5156
Yeji Moon (Nike) <yeaji21@gmail.com>
2022-03-02 07:43:11 +0900

    Revise comment

--- examples/Prototype/Main.kt
+++ examples/Prototype/Main.kt
@@ -26,7 +26,7 @@ data class Body(var arms: String, var legs: String)
 
 // Java의 최상위 개체인 Object에는 Cloneable이 protected로 정의되어 있기 때문에
 // 직접 만든 클래스에서 clone()을 "오버라이드" 하려면 Cloneable을 implement 해야 한다.
-// (그냥 자체적으로 clone() 메서드를 구현해도 되지만, 일반적으로는 오버라이드 해서 구현하는 편)
+// (그냥 자체적으로 clone() 메서드를 구현해도 되지만, Java/Kotlin을 사용할 때는 일반적으로 오버라이드 해서 구현하는 편)
 class Robot(val head: Head, val body: Body) : Cloneable {
     public override fun clone(): Robot {
         // Kotlin의 data class에서 제공하는 copy() 메서드를 활용함
