dc56c8f598f48e21d44ba56c1698716cf718fa6f
reid.lim <limsingt@naver.com>
2022-03-06 05:43:09 +0900

    change example

--- patterns/Decorator/examples/decorator_example_1.py
+++ patterns/Decorator/examples/decorator_example_1.py
@@ -2,52 +2,66 @@
 from abc import ABC, abstractmethod
 
 
-class Component(ABC):
+class TextConverter(ABC):
     @abstractmethod
-    def operation(self):
+    def operation(self, input_string: str) -> str:
         raise NotImplementedError()
 
 
-class ConcreteComponent(Component):
-    def operation(self):
-        print('crawling')
+class TextReverseConverter(TextConverter):
+    def operation(self, input_string: str) -> str:
+        print('[operation]')
+        return input_string[::-1]
 
 
-class BaseLogger(Component):
-    _component: Component = None
+class TextTwiceConverter(TextConverter):
+    def operation(self, input_string: str) -> str:
+        print('[operation]')
+        return input_string + input_string
 
-    def __init__(self, component: Component):
+
+class BaseLogger(TextConverter):
+    _component: TextConverter = None
+
+    def __init__(self, component: TextConverter):
         self._component = component
 
     @abstractmethod
-    def operation(self):
+    def operation(self, input_string: str) -> str:
         raise NotImplementedError()
 
 
-class StartLogger(BaseLogger):
-    def operation(self):
-        print('start operation')
-        self._component.operation()
+class InputLogger(BaseLogger):
+    def operation(self, input_string: str) -> str:
+        print(f'[start operation] input string: {input_string}')
+        return self._component.operation(input_string)
 
 
-class FinishLogger(BaseLogger):
-    def operation(self):
-        self._component.operation()
-        print('end operation')
+class ResultLogger(BaseLogger):
+    def operation(self, input_string: str) -> str:
+        operation_result = self._component.operation(input_string)
+        print(f'[finish operation] operation result: {operation_result}')
+        return operation_result
 
 
 if __name__ == "__main__":
-    component = ConcreteComponent()
-    component.operation()
-    # in operation
+    converter = TextReverseConverter()
+    converter.operation('I love you 3000')
+    # [operation]
 
-    component = StartLogger(component)
-    component.operation()
-    # start operation
-    # in operation
+    converter = InputLogger(converter)
+    converter.operation('I love you 3000')
+    # [start operation] input string: I love you 3000
+    # [operation]
 
-    component = FinishLogger(component)
-    component.operation()
-    # start operation
-    # in operation
-    # end operation
+    converter = ResultLogger(converter)
+    converter.operation('I love you 3000')
+    # [start operation] input string: I love you 3000
+    # [operation]
+    # [finish operation] operation result: 0003 uoy evol I
+
+    converter = TextTwiceConverter()
+    converter = ResultLogger(converter)
+    converter.operation('I love you 3000')
+    # [operation]
+    # [finish operation] operation result: I love you 3000I love you 3000
