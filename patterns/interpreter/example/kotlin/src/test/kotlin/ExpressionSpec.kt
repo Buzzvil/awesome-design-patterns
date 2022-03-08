4a694f0d6edd2769003fa3b31fbbb749dc1da720
bronn cha <agyomany@gmail.com>
2022-03-08 11:31:47 +0900

    OrExpression

--- patterns/interpreter/example/kotlin/src/test/kotlin/ExpressionSpec.kt
+++ patterns/interpreter/example/kotlin/src/test/kotlin/ExpressionSpec.kt
@@ -5,20 +5,18 @@ class TerminalExpressionSpec: BehaviorSpec({
     given("banana 라는 context 를 가진 TerminalExpression 이 있다.") {
         val bananaTerminalExpression = TerminalExpression("banana")
 
-        `when` ("context 와 동일한 data 가 주어진다.") {
-            val data = "banana"
+        `when` ("context 와 동일한 data 로 interpreter 한다"){
+            val result = bananaTerminalExpression.interpreter("banana")
 
             then("interpreter 는 context 와 data가 같은 경우 true 를 반환해야 한다") {
-                val result = bananaTerminalExpression.interpreter(data)
                 assertEquals(true, result)
             }
         }
 
-        `when` ("context 와 다른 data 가 주어진다.") {
-            val data = "apple"
+        `when` ("context 와 다른 data 로 interpreter 한다.") {
+            val result = bananaTerminalExpression.interpreter("apple")
 
             then("interpreter 는 context 와 data가 다른 경우 false 를 반환해야 한다") {
-                val result = bananaTerminalExpression.interpreter(data)
                 assertEquals(false, result)
             }
         }
@@ -26,8 +24,34 @@ class TerminalExpressionSpec: BehaviorSpec({
 
 })
 
+class OrExpressionSpec: BehaviorSpec({
+    given("2개의 TerminalExpression 이 주어진다. 해당 expression 들을 이용해 OrExpression 이 주어진다.") {
+        val fruit1 = TerminalExpression("apple")
+        val fruit2 = TerminalExpression("banana")
+        val orExpression = OrExpression(fruit1, fruit2)
+
+        `when` ("orExpression 이 포함한 data 를 interpreter 한다.") {
+            val result = orExpression.interpreter("banana")
+
+            then("data 가 orExpression 에 포함됐다면 true 를 return 해야 한다.") {
+                assertEquals(true, result)
+            }
+        }
+
+        `when` ("orExpression 이 포함하지 않은 data 를 interpreter 한다.") {
+            val result = orExpression.interpreter("kiwi")
+
+            then("data 가 orExpression 에 포함되지 않았다면 false 를 return 해야 한다.") {
+                assertEquals(false, result)
+            }
+        }
+    }
+
+})
+
+
 class ExpressionSpec: BehaviorSpec({
-    given("2개의 과일이 있다.") {
+    given("2개의 TerminalExpression 이 있다.") {
         val fruit1 = TerminalExpression("apple")
         val fruit2 = TerminalExpression("banana")
 
