82cd7c14039f5317d88b86fed68181b6a6564500
bronn cha <agyomany@gmail.com>
2022-03-25 19:11:46 +0900

    authenticationHandler

--- patterns/chain_of_responsibility/example/kotlin/src/main/kotlin/AuthenticationHandler.kt
+++ patterns/chain_of_responsibility/example/kotlin/src/main/kotlin/AuthenticationHandler.kt
@@ -2,4 +2,14 @@ class AuthenticationHandler: Handler {
     override fun setNext(handler: Handler): Handler {
         TODO("Not yet implemented")
     }
+
+    override fun handle(request: String): String? {
+        if (request.contains("authenticated")) {
+            return this.nextHandler.handle(request)
+        }
+        else {
+            return "Authentication fail"
+        }
+
+    }
 }
