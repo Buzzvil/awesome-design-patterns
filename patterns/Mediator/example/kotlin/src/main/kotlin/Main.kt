4f0cfccf27a95ffb31bcfd9ec40fe559c6481d7d
chaking <agyomany@gmail.com>
2022-06-14 11:42:01 +0900

    append result

--- patterns/Mediator/example/kotlin/src/main/kotlin/Main.kt
+++ patterns/Mediator/example/kotlin/src/main/kotlin/Main.kt
@@ -6,6 +6,11 @@ fun main(args: Array<String>) {
     val airplane = Airplane(mediator)
 
     glider.notifyTakeOff()
+    // glider: start taking off
+
     helicopter.notifyPassthrough()
+    // helicopter: passing through
+
     airplane.notifyLanding()
+    // airplane: waiting for landing
 }
